95c7e58497f15f548d2da069d2154898ba0beb3a
Craig Moore <cdm21d@fsu.edu>
2023-09-20 10:41:51 -0400

    Finished Step 2

--- sketch.js
+++ sketch.js
@@ -1,33 +1,67 @@
-var myParagraph;
+var myParagraph, myButton;
+x = 0;
+y = 0;
 
 function setup() {
   myParagraph = createP("html");
   myParagraph.position(0, 0);
-  myParagraph.mouseClicked(makeRed);
+
+  myButton = createButton("Click Me!");
+ 
 }
 
 function draw() {
-  myParagraph.position(frameCount % 200, 0);
+   myParagraph.position(mouseX, mouseY);
+   myButton.mouseClicked(congratulate);
 }
 
 function makeRed() {
   myParagraph.style("color", "red");
 }
 
+function congratulate() {
+   console.log("Congrats");
+   congratsP = createP("That was fantastic! Do it again");
+   congratsP.position(x, y);
+   count();
+}
+
+function count() {
+   (x += 20);
+   (y += 30)
+   console.log(x,y);
+}
+
 /*
 
 Some Questions:
 0. What is this percent sign all about? (hint: it's "modulus")
+      The percent sign is the remainder operator. For (x % y), it will output the remainder after x/y.
+      https://www.w3schools.com/jsrEF/jsref_operators.asp
+
 1. Where, in the p5 reference, do we find all this DOM stuff?
+      https://p5js.org/reference/#group-DOM for p5 specific DOM reference.
+      https://p5js.org/reference/#/p5.Element Reference for p5 Elements.
+      https://www.w3schools.com/jsrEF/default.asp for general JavaScript and HTML DOM.
+
 2. Why is the text so large when it's just a "paragraph"?
+      In style.css, the font size for 'p' is set to xx-large.
+
 3. "color" and "red"? Where did those come from and where 
    could you find more style attributes that could be
    changed?
-
+      These are standard CSS style properties, as outlined in any CSS reference.
+      https://www.w3schools.com/cssref/index.php
+      The p5.js style() function lets you control these CSS properties within the p5 document, with the benefit of being able to change the style in response to user interaction.
+      https://p5js.org/reference/#/p5.Element/style
+      
 Some Things to Try:
 0. Make the text follow the mouse pointer
+      Done
 1. Make a button
+      Done
 2. Make the button affect the style or position of some text
+      Done
 3. Make a picture that shows up on the page. Does it have to
    be created in p5 or can you just place it in the HTML?
    What's the difference?
